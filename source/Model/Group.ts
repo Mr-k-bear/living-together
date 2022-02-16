f999fc12682d379734bfd20290722e512c43a285
MrKBear <mrkbear@qq.com>
2022-02-16 21:58:28 +0800

    Add modular

--- source/Model/Group.ts
+++ source/Model/Group.ts
@@ -1,9 +1,23 @@
+import { Individual } from "./Individual";
 
 /**
  * 群体类型
  */
 class Group {
 
+	/**
+	 * 所有成员
+	 */
+	public individuals: Individual[] = [];
+
+	/**
+	 * 添加一个成员
+	 */
+	public add(count: number = 1) {
+		for (let i = 0; i < count; i++) {
+			this.individuals.push(new Individual(this));
+		}
+	}
 }
 
 export default Group;
--- source/Model/Individual.ts
+++ source/Model/Individual.ts
@@ -1,9 +1,26 @@
+import type { Group } from "./Group";
 
 /**
  * 群中的个体类型
  */
 class Individual {
 
+	/**
+	 * 坐标
+	 */
+	public position: number[] = [0, 0, 0];
+
+	/**
+	 * 所属群组
+	 */
+	public group: Group;
+
+	/**
+	 * 初始化
+	 */
+	public constructor(group: Group) {
+		this.group = group;
+	}
 }
 
 export default Individual;
