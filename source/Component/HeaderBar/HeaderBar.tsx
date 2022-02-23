0eb0b6f7f560ae44c9d56eeae25257b55d655008
MrKBear <mrkbear@qq.com>
2022-02-23 17:51:20 +0800

    Test Consumer Provider

---
+++ source/Component/HeaderBar/HeaderBar.tsx
@@ -0,0 +1,44 @@
+import { Component, ReactNode } from "react";
+import { useSetting, IMixinSettingProps, Themes } from "@Context/Setting";
+
+interface IHeaderBarProps {}
+
+/**
+ * 头部信息栏
+ */
+@useSetting
+class HeaderBar extends Component<IHeaderBarProps & IMixinSettingProps> {
+
+    private handelClick = () => {
+        if (this.props.setting) {
+            this.props.setting.setProps("themes", 
+                this.props.setting.themes === Themes.dark ? Themes.light : Themes.dark
+            );
+        }
+    }
+
+    private changeListener = () => {
+        this.forceUpdate();
+    }
+
+    public componentDidMount() {
+        console.log("mount");
+        if (this.props.setting) {
+            this.props.setting.on("change", this.changeListener);
+        }
+    }
+
+    public componentWillUnmount() {
+        console.log("die");
+        if (this.props.setting) {
+            this.props.setting.off("change", this.changeListener);
+        }
+    }
+
+    public render(): ReactNode {
+        return <div onClick={this.handelClick}>{this.props.setting?.themes}</div>
+    }
+}
+
+export default HeaderBar;
+export { HeaderBar };
--- source/Page/SimulatorWeb/SimulatorWeb.tsx
+++ source/Page/SimulatorWeb/SimulatorWeb.tsx
@@ -1,13 +1,28 @@
-import { Component, ReactNode, createRef } from "react";
+import { Component, ReactNode } from "react";
+import { SettingProvider, Setting } from "@Context/Setting";
+import { HeaderBar } from "@Component/HeaderBar/HeaderBar";
 import { Entry } from "../Entry/Entry";
 import "./SimulatorWeb.scss";
 
 class SimulatorWeb extends Component {
     
-    private canvasContRef = createRef<HTMLDivElement>();
+    /**
+     * 全局设置
+     */
+    private setting: Setting;
+
+    public constructor(props: any) {
+        super(props);
+
+        // TODO: 这里要读取设置
+        this.setting = new Setting();
+        (window as any).setting = (this.setting as any);
+    }
 
     public render(): ReactNode {
-        return <div>Web</div>
+        return <SettingProvider value={this.setting}>
+            <HeaderBar/>
+        </SettingProvider>
     }
 }
 
