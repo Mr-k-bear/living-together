851ca149b9ec9f2cbb0af5bee6fdc5a26f80e12b
MrKBear <mrkbear@qq.com>
2022-03-17 22:47:33 +0800

    Optim object list checkout icon

--- source/Component/DetailsList/DetailsList.tsx
+++ source/Component/DetailsList/DetailsList.tsx
@@ -23,9 +23,12 @@ interface IDetailsListProps {
     click?: () => void;
     clickLine?: (item: IItems) => any;
     checkBox?: (data: IItems) => any;
+    renderCheckbox?: (data: IItems, click: () => void) => ReactNode;
 }
 
 class DetailsList extends Component<IDetailsListProps> {
+
+    private propagationCount = 0;
 
     private renderValue<D extends IItems, K extends keyof D>(item: IItems, column: IColumns<D, K>) {
         const classList: string[] = [];
@@ -43,15 +46,36 @@ class DetailsList extends Component<IDetailsListProps> {
         </div>
     }
 
+    private handelClickCheckbox(item: IItems) {
+        if (this.propagationCount <= 2 && this.props.checkBox) {
+            this.props.checkBox(item);
+            this.propagationCount = 2;
+        }
+    }
+
+    private renderCheckbox(item: IItems) {
+        const { checkboxClassName } = this.props;
+        return <div 
+            className={"details-list-checkbox" + (checkboxClassName ? ` ${checkboxClassName}` : "")}
+            onClick={() => this.handelClickCheckbox(item)}
+        >
+            <Icon iconName="CheckMark"></Icon>
+        </div>;
+    }
+
     public render(): ReactNode {
         return <Theme
             className={"details-list" + (this.props.className ? ` ${this.props.className}` : "")}
-            onClick={this.props.click}
+            onClick={() => {
+                if (this.propagationCount <= 0 && this.props.click) {
+                    this.props.click();
+                }
+                this.propagationCount = 0;
+            }}
             backgroundLevel={BackgroundLevel.Level4}
             fontLevel={FontLevel.normal}
         >{
             this.props.items.map((item) => {
-                const { checkboxClassName } = this.props;
                 const classList: string[] = ["details-list-item"];
                 if (item.select) {
                     classList.push("active");
@@ -59,10 +83,10 @@ class DetailsList extends Component<IDetailsListProps> {
                 return <div
                     className={classList.join(" ")}
                     key={item.key}
-                    onClick={(e) => {
-                        if (this.props.clickLine) {
-                            e.stopPropagation();
+                    onClick={() => {
+                        if (this.propagationCount <= 1 && this.props.clickLine) {
                             this.props.clickLine(item);
+                            this.propagationCount = 1;
                         }
                     }}
                 >
@@ -74,18 +98,10 @@ class DetailsList extends Component<IDetailsListProps> {
                         })
                     }
                     {
-                        this.props.hideCheckBox ? null :
-                        <div 
-                            className={"details-list-checkbox" + (checkboxClassName ? ` ${checkboxClassName}` : "")}
-                            onClick={(e) => {
-                                if (this.props.checkBox) {
-                                    e.stopPropagation();
-                                    this.props.checkBox(item);
-                                }
-                            }}
-                        >
-                            <Icon iconName="CheckMark"></Icon>
-                        </div>
+                        this.props.hideCheckBox ? null : 
+                            this.props.renderCheckbox ? 
+                                this.props.renderCheckbox(item, () => this.handelClickCheckbox(item)) :
+                                this.renderCheckbox(item)
                     }
                     {
                         this.props.columns.map((column) => {
