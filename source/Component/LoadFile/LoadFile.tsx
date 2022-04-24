4137362501e8dfc9edf5447fd46b560a8cfd25c1
MrKBear <mrkbear@qq.com>
2022-04-24 20:16:03 +0800

    Add drag layer

--- source/Component/LoadFile/LoadFile.tsx
+++ source/Component/LoadFile/LoadFile.tsx
@@ -4,27 +4,65 @@ import { Icon } from "@fluentui/react";
 import { Component, ReactNode } from "react";
 import "./LoadFile.scss";
 
-class LoadFile extends Component {
+interface ILoadFileState {
+	show: boolean;
+}
+
+class LoadFile extends Component<{}, ILoadFileState> {
+
+	public state: Readonly<ILoadFileState> = {
+		show: false
+	};
 
 	private renderMask() {
 		return <Theme
 			className="load-file-layer-root"
 			fontLevel={FontLevel.normal}
+			onDragEnter={this.handleWindowsDragOnFiles}
+			onDragLeave={this.handleWindowsDragOffFiles}
 		>
-			<div className="drag-icon">
-				<Icon iconName="KnowledgeArticle"/>
-			</div>
-			<div className="drag-title">
-				<Localization i18nKey="Info.Hint.Load.File.Title"/>
-			</div>
-			<div className="drag-intro">
-				<Localization i18nKey="Info.Hint.Load.File.Intro"/>
+			<div className="load-file-layer">
+				<div className="drag-icon">
+					<Icon iconName="KnowledgeArticle"/>
+				</div>
+				<div className="drag-title">
+					<Localization i18nKey="Info.Hint.Load.File.Title"/>
+				</div>
+				<div className="drag-intro">
+					<Localization i18nKey="Info.Hint.Load.File.Intro"/>
+				</div>
 			</div>
 		</Theme>;
 	}
 
+	private offDragTimer: NodeJS.Timeout | undefined;
+
+	private handleWindowsDragOnFiles = () => {
+		clearTimeout(this.offDragTimer as number | undefined);
+		this.setState({
+			show: true
+		});
+	}
+
+	private handleWindowsDragOffFiles = () => {
+		clearTimeout(this.offDragTimer as number | undefined);
+		this.offDragTimer = setTimeout(() => {
+			this.setState({
+				show: false
+			});
+		});
+	}
+
+	public componentDidMount() {
+		window.addEventListener("dragenter", this.handleWindowsDragOnFiles);
+	}
+
+	public componentWillUnmount() {
+		window.removeEventListener("dragenter", this.handleWindowsDragOnFiles);
+	}
+
 	public render(): ReactNode {
-		return <></>;
+		return this.state.show ? this.renderMask() : null;
 	}
 }
 
